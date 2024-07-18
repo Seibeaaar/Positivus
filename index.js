08058eabe487d76f8edc377f4aec9aa152688c11
Seibeaaar <yuribar4520@gmail.com>
2024-07-18 16:36:50 +0300

    Add working process section

---
+++ index.js
@@ -0,0 +1,45 @@
+const INACTIVE_CLASS = 'process-item__info--inactive';
+const GRAY_BACKGROUND_CLASS = 'gray-background';
+const PRIMARY_BACKGROUND_CLASS = 'primary-background';
+
+const PLUS_ICON_SRC = '/assets/icons/Plus.svg';
+const MINUS_ICON_SRC = '/assets/icons/Minus.svg';
+
+
+const workProcessItems = document.querySelectorAll('.process-item');
+
+const toggleCSSClass = (e, className, alternative) => {
+    const classNameUsed = e.classList.contains(className);
+    classNameUsed ? e.classList.remove(className) : e.classList.add(className);
+
+    if (!!alternative) {
+        classNameUsed ? e.classList.add(alternative) : e.classList.remove(alternative);
+    }
+};
+
+const handleWorkProcessClick = (event, container) => {
+    if (!event.target || !['toggle-button', 'toggle-icon'].includes(event.target.accessKey)) return;
+    const processInfo = container.querySelector('.process-item__info');
+    toggleCSSClass(processInfo, INACTIVE_CLASS);
+
+    const includesInactiveClass = processInfo.classList.contains(INACTIVE_CLASS);
+
+    if (includesInactiveClass) {
+        processInfo.style.height = 0;
+    } else {
+        const infoText = processInfo.firstElementChild;
+        const textHeight = infoText.scrollHeight;
+        processInfo.style.height = `${textHeight + 40}px`;
+    }
+
+    if (event.target.accessKey === 'toggle-icon') {
+        event.target.src = includesInactiveClass ? PLUS_ICON_SRC : MINUS_ICON_SRC;
+    } else {
+        const toggleIcon = event.target.firstElementChild;
+        toggleIcon.src = includesInactiveClass ? PLUS_ICON_SRC : MINUS_ICON_SRC;
+    }
+
+    toggleCSSClass(container, GRAY_BACKGROUND_CLASS, PRIMARY_BACKGROUND_CLASS);
+};
+
+workProcessItems.forEach(item => item.addEventListener('click', (e) => handleWorkProcessClick(e, item)));
