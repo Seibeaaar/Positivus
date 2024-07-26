3059121c3b172d8c263007da6e50fe97ec49fbc2
Seibeaaar <yuribar4520@gmail.com>
2024-07-26 15:03:54 +0300

    Add mobile navbar

--- js/mobileNavMenu.js
+++ js/mobileNavMenu.js
@@ -2,12 +2,26 @@ import { toggleCSSClass } from "./utils.js";
 
 const burgerMenu = document.querySelector('.header-burger');
 const menuLines = document.querySelectorAll('.header-burger__line');
+const nav = document.querySelector('.mobile-navigation');
+const overlay = document.querySelector('.mobile-navigation__overlay');
+const navLinks = nav.querySelectorAll('a');
 
-burgerMenu.addEventListener('click', () => {
-    const isActive = burgerMenu.classList.contains('header-burger--active');
-
+const toggleNavMenu = () => {
     toggleCSSClass(burgerMenu, 'header-burger--active');
     toggleCSSClass(menuLines.item(0), 'header-burger__line--top');
     toggleCSSClass(menuLines.item(1), 'header-burger__line--middle');
     toggleCSSClass(menuLines.item(2), 'header-burger__line--bottom');
-});
+    const isNavOpen = nav.classList.contains('mobile-navigation--active');
+    if (!isNavOpen) {
+        document.body.style.overflow = 'hidden';
+        nav.style.height = `${nav.scrollHeight + 80}px`;
+    } else {
+        document.body.style.overflow = 'auto';
+        nav.style.height = 0;
+    }
+    toggleCSSClass(nav, 'mobile-navigation--active', 'mobile-navigation--inactive');
+    toggleCSSClass(overlay, 'mobile-navigation__overlay--active');
+}
+
+burgerMenu.addEventListener('click', toggleNavMenu);
+navLinks.forEach(link => link.addEventListener('click', toggleNavMenu));
